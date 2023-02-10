1e1694a6f82ef512185a9e1af0a845e620e32fc1
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-02-10 11:43:20 +0100

    mlp implemented

---
+++ nn.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+
+class MLP(nn.Module):
+    def __init__(self, input_size, output_size, hidden_layers, n_neurons) -> None:
+        super(MLP, self).__init__()
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(input_size, n_neurons))
+        self.layers.append(nn.LeakyReLU())
+
+        for _ in range(hidden_layers):
+            self.layers.append(nn.Linear(n_neurons, n_neurons))
+            self.layers.append(nn.LeakyReLU())
+
+        self.layers.append(nn.Linear(n_neurons, output_size))
+
+    def forward(self, x):
+        y = x
+        for layer in self.layers:
+            y = layer(y)
+        return y
+
+
