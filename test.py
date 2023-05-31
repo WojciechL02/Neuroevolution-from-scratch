4b70844215f3ff7971646fe0c7b08c80e3dc5046
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-06-01 00:16:03 +0200

    method for testing the NN

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import torch
 
 
-def test(model, device, criterion, test_loader):
+def test(model, device, test_loader):
     model.eval()
-    test_loss = 0
+    # test_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             data = data.view(len(data), -1)
             output = model(data)
-            test_loss += criterion(output, target.long())
+            # test_loss += criterion(output, target.long())
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    print(f"\nTest set: Loss: {test_loss}")
+    # print(f"\nTest set: Loss: {test_loss}")
     print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
         correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
